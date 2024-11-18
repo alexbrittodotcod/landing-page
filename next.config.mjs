5c6bf9aa43189a78bb27b21fec052eb7d4db043e
Alex Britto <alexbritto@dotcod.in>
2024-11-18 15:06:44 +0530

    UI update

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: "/landing-page", // Replace with your desired base path
+  assetPrefix: "/landing-page/", // Set the asset prefix
   reactStrictMode: true,
   // Enables detailed source maps in development
   productionBrowserSourceMaps: true,
